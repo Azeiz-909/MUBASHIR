23191fa0490c59de45175207a2651afe69e06958
Azeiz-909 <harbigoat3@gmail.com>
2026-08-25 07:24:06 +0300

    Delete api/auth/logout.js

--- api/auth/logout.js
+++
@@ -1,7 +0,0 @@
-const { clearSessionCookie } = require('../_lib/auth');
-
-module.exports = async (req, res) => {
-  if (req.method !== 'POST') return res.status(405).json({ error: 'طريقة غير مدعومة' });
-  clearSessionCookie(res);
-  res.status(200).json({ ok: true });
-};
